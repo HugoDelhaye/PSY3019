99e5f07c182737b452d1af123a8dca0f0340047d
Hugo Delhaye <hugo.delhaye@umontreal.ca>
2024-02-06 10:53:59 -0500

    Figures matplot

--- FigureHugo.py
+++ FigureHugo.py
@@ -4,8 +4,8 @@ import os
 import pandas as pd
 
 # définir le chemin
-chemin_main = os.path.dirname(os.path.abspath(__file__)
-chemin = os.path.join(chemin_main, 'figureHugo.png')
+chemin_main = os.path.dirname(os.path.abspath(__file__))
+chemin = os.path.join(chemin_main, 'resultats','figureHugo.png')
 
 # visualiser les données
 dat = np.array([1, 2, 4, 8, 16, 32])
@@ -19,7 +19,7 @@ fig.savefig(chemin)
 
 # importer mon cadre de données
 df = pd.read_csv('HugoDelhaye_psy6973_data_20240116.csv')
-df2 = df[['l_striatum']]replace('.', np.nan)
+df2 = df[['l_striatum']].replace('.', np.nan)
 arr = df2.to_numpy(dtype = np.float64)
 
 
@@ -28,6 +28,22 @@ plt.plot(arr, 'r^')
 plt.show()
 
 # sauvegarder la figure
-chemin2 = os.path.join(chemin_main, 'figureDonneesHugo.png')
+chemin2 = os.path.join(chemin_main, 'resultats','figureDonneesHugo.png')
 fig = plt.figure()
 fig.savefig(chemin2)
+plt.close()
+
+#polar 
+
+N = arr.size
+r = 2 * np.random.rand(N)
+area = 200 * r**2
+theta = 2 * np.pi * np.random.rand(N)
+
+fig = plt.figure()
+ax  = fig.add_subplot(111, projection='polar')
+c   = ax.scatter(theta, arr, s=area, cmap='hsv', alpha=0.75)
+
+chemin3 = os.path.join(chemin_main, 'resultats','figureDonneesHugo.png')
+fig.savefig(chemin3)
+plt.close()
