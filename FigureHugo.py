ab4ee184b1d0d7cf68446c822b45510e718f1ac6
Hugo Delhaye <hugo.delhaye@umontreal.ca>
2024-02-06 10:14:23 -0500

    Figures matplot

--- FigureHugo.py
+++ FigureHugo.py
@@ -1,15 +1,33 @@
 from matplotlib import pyplot as plt
-import numpy
+import numpy as np
 import os
+import pandas as pd
 
 # définir le chemin
-chemin = os.path.join(os.environ['HOME'],'Desktop')
+chemin_main = os.path.dirname(os.path.abspath(__file__)
+chemin = os.path.join(chemin_main, 'figureHugo.png')
 
 # visualiser les données
 dat = np.array([1, 2, 4, 8, 16, 32])
 plt.plot(dat, '.', color = '#000099' )
 plt.show()
+plt.close()
 
 # sauvegarder la figure
 fig = plt.figure() # pour sauvegarder, il faut créer une figure
-fig.savefig(os.path.join(chemin, 'figureHugo.png'))
+fig.savefig(chemin)
+
+# importer mon cadre de données
+df = pd.read_csv('HugoDelhaye_psy6973_data_20240116.csv')
+df2 = df[['l_striatum']]replace('.', np.nan)
+arr = df2.to_numpy(dtype = np.float64)
+
+
+# visualiser les données
+plt.plot(arr, 'r^')
+plt.show()
+
+# sauvegarder la figure
+chemin2 = os.path.join(chemin_main, 'figureDonneesHugo.png')
+fig = plt.figure()
+fig.savefig(chemin2)
