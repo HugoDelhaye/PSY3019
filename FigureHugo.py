1d32f119366464c131daed5baad293a300600770
Hugo Delhaye <hugo.delhaye@umontreal.ca>
2024-02-06 09:36:57 -0500

    Figure matplot

--- FigureHugo.py
+++ FigureHugo.py
@@ -1,7 +1,15 @@
 from matplotlib import pyplot as plt
+import numpy
+import os
+
+# définir le chemin
+chemin = os.path.join(os.environ['HOME'],'Desktop')
+
+# visualiser les données
 dat = np.array([1, 2, 4, 8, 16, 32])
 plt.plot(dat, '.', color = '#000099' )
 plt.show
 
+# sauvegarder la figure
 fig = plt.figure() # pour sauvegarder, il faut créer une figure
-fig.savefig(os.path.join(chemin, 'my_figure.png'))
+fig.savefig(os.path.join(chemin, 'figureHugo.png'))
