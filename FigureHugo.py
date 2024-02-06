62b82fb79662f3f37434eb6f6348c5c1fd2af12c
Hugo Delhaye <hugo.delhaye@umontreal.ca>
2024-02-06 09:38:09 -0500

    Figure matplot

--- FigureHugo.py
+++ FigureHugo.py
@@ -8,7 +8,7 @@ chemin = os.path.join(os.environ['HOME'],'Desktop')
 # visualiser les données
 dat = np.array([1, 2, 4, 8, 16, 32])
 plt.plot(dat, '.', color = '#000099' )
-plt.show
+plt.show()
 
 # sauvegarder la figure
 fig = plt.figure() # pour sauvegarder, il faut créer une figure
